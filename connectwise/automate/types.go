e261183909b4fc555e1a74f718225e73090fdcf5
Danny Rosenthal <dsrosen6@gmail.com>
2026-06-26 08:24:54 -0500

    fix missing fields

--- connectwise/automate/types.go
+++ connectwise/automate/types.go
@@ -30,30 +30,37 @@ type Company struct {
 	Locations                     []Location `json:"Locations,omitempty"`
 }
 
-// Location is an Automate client location (LabTech.Models.Location). Deeply
-// nested objects (contact, router, deployment templates, etc.) are kept as raw
-// JSON in this scaffold; type them out as needed.
+// Location is an Automate client location (LabTech.Models.Location for
+// POST/PUT request bodies, Automate.Api.Domain.Contracts.Clients.Location for
+// GET responses). Deeply nested objects (contact, router, deployment
+// templates, etc.) are kept as raw JSON in this scaffold; type them out as
+// needed.
+//
+// Client must be set when creating a location (POST) so the new location is
+// associated with a client; setting only Client.ID is sufficient.
 type Location struct {
-	ID                  int    `json:"Id,omitempty"`
-	Name                string `json:"Name,omitempty"`
-	Address1            string `json:"Address1,omitempty"`
-	Address2            string `json:"Address2,omitempty"`
-	City                string `json:"City,omitempty"`
-	State               string `json:"State,omitempty"`
-	ZipCode             string `json:"ZipCode,omitempty"`
-	Country             string `json:"Country,omitempty"`
-	PhoneNumber         string `json:"PhoneNumber,omitempty"`
-	FaxNumber           string `json:"FaxNumber,omitempty"`
-	Comments            string `json:"Comments,omitempty"`
-	RouterPort          int    `json:"RouterPort,omitempty"`
-	ScriptDrive         string `json:"ScriptDrive,omitempty"`
-	ScriptUsername      string `json:"ScriptUsername,omitempty"`
-	ScriptPassword      string `json:"ScriptPassword,omitempty"`
-	ScriptRouterAddress string `json:"ScriptRouterAddress,omitempty"`
-	ScriptExtra1        string `json:"ScriptExtra1,omitempty"`
-	ScriptExtra2        string `json:"ScriptExtra2,omitempty"`
-	ProbeId             int    `json:"ProbeId,omitempty"`
-	ExternalId          int    `json:"ExternalId,omitempty"`
+	ID                  int      `json:"Id,omitempty"`
+	LocationId          int      `json:"LocationId,omitempty"`
+	Client              *Company `json:"Client,omitempty"`
+	Name                string   `json:"Name,omitempty"`
+	Address1            string   `json:"Address1,omitempty"`
+	Address2            string   `json:"Address2,omitempty"`
+	City                string   `json:"City,omitempty"`
+	State               string   `json:"State,omitempty"`
+	ZipCode             string   `json:"ZipCode,omitempty"`
+	Country             string   `json:"Country,omitempty"`
+	PhoneNumber         string   `json:"PhoneNumber,omitempty"`
+	FaxNumber           string   `json:"FaxNumber,omitempty"`
+	Comments            string   `json:"Comments,omitempty"`
+	RouterPort          int      `json:"RouterPort,omitempty"`
+	ScriptDrive         string   `json:"ScriptDrive,omitempty"`
+	ScriptUsername      string   `json:"ScriptUsername,omitempty"`
+	ScriptPassword      string   `json:"ScriptPassword,omitempty"`
+	ScriptRouterAddress string   `json:"ScriptRouterAddress,omitempty"`
+	ScriptExtra1        string   `json:"ScriptExtra1,omitempty"`
+	ScriptExtra2        string   `json:"ScriptExtra2,omitempty"`
+	ProbeId             int      `json:"ProbeId,omitempty"`
+	ExternalId          int      `json:"ExternalId,omitempty"`
 
 	Contact                json.RawMessage `json:"Contact,omitempty"`
 	Router                 json.RawMessage `json:"Router,omitempty"`
@@ -61,6 +68,10 @@ type Location struct {
 	MaintenanceWindow      json.RawMessage `json:"MaintenanceWindow,omitempty"`
 	DefaultDeploymentGroup json.RawMessage `json:"DefaultDeploymentGroup,omitempty"`
 	DefaultDeploymentLogin json.RawMessage `json:"DefaultDeploymentLogin,omitempty"`
+
+	// ExtraFields is returned on GET; manage EDFs via the dedicated
+	// /Locations/{id}/ExtraFields endpoints (see locations.go).
+	ExtraFields []ExtraField `json:"ExtraFields,omitempty"`
 }
 
 // ExtraField is a custom field on a client
@@ -129,31 +140,67 @@ type Computer struct {
 	MACAddress               string    `json:"MACAddress,omitempty"`
 	RemoteAgentVersion       string    `json:"RemoteAgentVersion,omitempty"`
 	BiosManufacturer         string    `json:"BiosManufacturer,omitempty"`
+	BiosFlash                string    `json:"BiosFlash,omitempty"`
 	UTCOffset                int       `json:"UTCOffset,omitempty"`
 	TotalMemory              int64     `json:"TotalMemory,omitempty"`
 	FreeMemory               int64     `json:"FreeMemory,omitempty"`
 	CpuUsage                 int       `json:"CpuUsage,omitempty"`
 	SystemUptime             int64     `json:"SystemUptime,omitempty"`
+	UserIdleTime             int       `json:"UserIdleTime,omitempty"`
+	Bandwidth                int       `json:"Bandwidth,omitempty"`
+	BandwidthDisplay         string    `json:"BandwidthDisplay,omitempty"`
+	MasterMode               string    `json:"MasterMode,omitempty"`
+	TempFiles                string    `json:"TempFiles,omitempty"`
+	CurrentPowerProfile      string    `json:"CurrentPowerProfile,omitempty"`
+	CpuScore                 float64   `json:"CpuScore,omitempty"`
+	D3DScore                 float64   `json:"D3DScore,omitempty"`
+	DiskScore                float64   `json:"DiskScore,omitempty"`
+	GraphicsScore            float64   `json:"GraphicsScore,omitempty"`
+	MemoryScore              float64   `json:"MemoryScore,omitempty"`
 	IsFasTalk                bool      `json:"IsFasTalk,omitempty"`
 	IsMaster                 bool      `json:"IsMaster,omitempty"`
 	IsNetworkProbe           bool      `json:"IsNetworkProbe,omitempty"`
+	IsHeartbeatEnabled       bool      `json:"IsHeartbeatEnabled,omitempty"`
+	IsHeartbeatRunning       bool      `json:"IsHeartbeatRunning,omitempty"`
+	IsTunnelSupported        bool      `json:"IsTunnelSupported,omitempty"`
 	IsMaintenanceModeEnabled bool      `json:"IsMaintenanceModeEnabled,omitempty"`
 	IsVirtualMachine         bool      `json:"IsVirtualMachine,omitempty"`
+	IsVirtualHost            bool      `json:"IsVirtualHost,omitempty"`
+	IsLockedDown             bool      `json:"IsLockedDown,omitempty"`
+	IsSystemAccount          bool      `json:"IsSystemAccount,omitempty"`
 	IsRebootNeeded           bool      `json:"IsRebootNeeded,omitempty"`
+	HasIntelVPRO             bool      `json:"HasIntelVPRO,omitempty"`
+	HasIntelAMT              bool      `json:"HasIntelAMT,omitempty"`
+	HasHPiLO                 bool      `json:"HasHPiLO,omitempty"`
 	RemoteAgentLastContact   time.Time `json:"RemoteAgentLastContact,omitzero"`
+	RemoteAgentLastInventory time.Time `json:"RemoteAgentLastInventory,omitzero"`
 	LastInventoryReceived    time.Time `json:"LastInventoryReceived,omitzero"`
+	WindowsUpdateDate        time.Time `json:"WindowsUpdateDate,omitzero"`
+	AntivirusDefinitionDate  time.Time `json:"AntivirusDefinitionDate,omitzero"`
 	LastHeartbeat            time.Time `json:"LastHeartbeat,omitzero"`
 	LastStartup              time.Time `json:"LastStartup,omitzero"`
 	DateAdded                time.Time `json:"DateAdded,omitzero"`
+	AssetDate                time.Time `json:"AssetDate,omitzero"`
 	WarrantyEndDate          time.Time `json:"WarrantyEndDate,omitzero"`
 
-	Location      json.RawMessage `json:"Location,omitempty"`
-	Client        json.RawMessage `json:"Client,omitempty"`
-	Contact       json.RawMessage `json:"Contact,omitempty"`
-	VirusScanner  json.RawMessage `json:"VirusScanner,omitempty"`
-	LoggedInUsers json.RawMessage `json:"LoggedInUsers,omitempty"`
-	Tickets       json.RawMessage `json:"Tickets,omitempty"`
-	Groups        json.RawMessage `json:"Groups,omitempty"`
+	OpenPortsTCP      []int    `json:"OpenPortsTCP,omitempty"`
+	OpenPortsUDP      []int    `json:"OpenPortsUDP,omitempty"`
+	DomainNameServers []string `json:"DomainNameServers,omitempty"`
+	PowerProfiles     []string `json:"PowerProfiles,omitempty"`
+	HardwarePorts     []string `json:"HardwarePorts,omitempty"`
+	UserAccounts      []string `json:"UserAccounts,omitempty"`
+	IRQ               []int    `json:"IRQ,omitempty"`
+	Address           []int    `json:"Address,omitempty"`
+	DMA               []int    `json:"DMA,omitempty"`
+
+	Location        json.RawMessage `json:"Location,omitempty"`
+	Client          json.RawMessage `json:"Client,omitempty"`
+	Contact         json.RawMessage `json:"Contact,omitempty"`
+	VirusScanner    json.RawMessage `json:"VirusScanner,omitempty"`
+	CommentPriority json.RawMessage `json:"CommentPriority,omitempty"`
+	LoggedInUsers   json.RawMessage `json:"LoggedInUsers,omitempty"`
+	Tickets         json.RawMessage `json:"Tickets,omitempty"`
+	Groups          json.RawMessage `json:"Groups,omitempty"`
 }
 
 // ComputerDevice is a hardware device on a computer (LabTech.Models.ComputerDevice).
@@ -206,44 +253,66 @@ type ComputerSoftware struct {
 // ComputerOperatingSystem describes a computer's OS
 // (LabTech.Models.ComputerOperatingSystem).
 type ComputerOperatingSystem struct {
-	ComputerId         int       `json:"ComputerId,omitempty"`
-	Name               string    `json:"Name,omitempty"`
-	MajorVersion       int       `json:"MajorVersion,omitempty"`
-	MinorVersion       int       `json:"MinorVersion,omitempty"`
-	Version            string    `json:"Version,omitempty"`
-	DotNetVersion      string    `json:"DotNetVersion,omitempty"`
-	ServicePack        string    `json:"ServicePack,omitempty"`
-	ServicePackName    string    `json:"ServicePackName,omitempty"`
-	IsLicensed         bool      `json:"IsLicensed,omitempty"`
-	BaseFolder         string    `json:"BaseFolder,omitempty"`
-	SystemDrive        string    `json:"SystemDrive,omitempty"`
-	HasGui             bool      `json:"HasGui,omitempty"`
-	Is64Bit            bool      `json:"Is64Bit,omitempty"`
-	Domain             string    `json:"Domain,omitempty"`
-	IsDomainController bool      `json:"IsDomainController,omitempty"`
-	IsServer           bool      `json:"IsServer,omitempty"`
-	InstallDate        time.Time `json:"InstallDate,omitzero"`
-	DateUpdated        time.Time `json:"DateUpdated,omitzero"`
-	ReleaseId          int       `json:"ReleaseId,omitempty"`
-	Edition            string    `json:"Edition,omitempty"`
+	ComputerId           int       `json:"ComputerId,omitempty"`
+	Name                 string    `json:"Name,omitempty"`
+	MajorVersion         int       `json:"MajorVersion,omitempty"`
+	MinorVersion         int       `json:"MinorVersion,omitempty"`
+	Version              string    `json:"Version,omitempty"`
+	DotNetVersion        string    `json:"DotNetVersion,omitempty"`
+	ServicePack          string    `json:"ServicePack,omitempty"`
+	ServicePackName      string    `json:"ServicePackName,omitempty"`
+	ProductType          int       `json:"ProductType,omitempty"`
+	Suite                int       `json:"Suite,omitempty"`
+	ProductInfo          int       `json:"ProductInfo,omitempty"`
+	IsLicensed           bool      `json:"IsLicensed,omitempty"`
+	IsTablet             bool      `json:"IsTablet,omitempty"`
+	IsStarter            bool      `json:"IsStarter,omitempty"`
+	IsMediaCenter        bool      `json:"IsMediaCenter,omitempty"`
+	BaseFolder           string    `json:"BaseFolder,omitempty"`
+	SystemDrive          string    `json:"SystemDrive,omitempty"`
+	HasGui               bool      `json:"HasGui,omitempty"`
+	Is64Bit              bool      `json:"Is64Bit,omitempty"`
+	Domain               string    `json:"Domain,omitempty"`
+	IsDomainController   bool      `json:"IsDomainController,omitempty"`
+	IsServer             bool      `json:"IsServer,omitempty"`
+	InstallDate          time.Time `json:"InstallDate,omitzero"`
+	DateUpdated          time.Time `json:"DateUpdated,omitzero"`
+	ReleaseId            int       `json:"ReleaseId,omitempty"`
+	Edition              string    `json:"Edition,omitempty"`
+	BranchReadinessLevel int       `json:"BranchReadinessLevel,omitempty"`
 }
 
 // ComputerBios describes a computer's BIOS (LabTech.Models.ComputerBios).
 type ComputerBios struct {
-	ComputerId       int       `json:"ComputerId,omitempty"`
-	Vendor           string    `json:"Vendor,omitempty"`
-	Version          string    `json:"Version,omitempty"`
-	Date             time.Time `json:"Date,omitzero"`
-	Size             int       `json:"Size,omitempty"`
-	SmBiosVersion    float64   `json:"SmBiosVersion,omitempty"`
-	SupportsUefi     bool      `json:"SupportsUefi,omitempty"`
-	IsVirtualMachine bool      `json:"IsVirtualMachine,omitempty"`
-	PowerOnReason    string    `json:"PowerOnReason,omitempty"`
-	IsPortable       bool      `json:"IsPortable,omitempty"`
-	VmHost           string    `json:"VmHost,omitempty"`
-	VmType           string    `json:"VmType,omitempty"`
-	VmName           string    `json:"VmName,omitempty"`
-	DateUpdated      time.Time `json:"DateUpdated,omitzero"`
+	ComputerId        int       `json:"ComputerId,omitempty"`
+	Vendor            string    `json:"Vendor,omitempty"`
+	Version           string    `json:"Version,omitempty"`
+	Date              time.Time `json:"Date,omitzero"`
+	Size              int       `json:"Size,omitempty"`
+	SystemBiosVersion float64   `json:"SystemBiosVersion,omitempty"`
+	EMBCTLVersion     float64   `json:"EMBCTLVersion,omitempty"`
+	SmBiosVersion     float64   `json:"SmBiosVersion,omitempty"`
+	DmiVersion        int       `json:"DmiVersion,omitempty"`
+	BiosChar          int       `json:"BiosChar,omitempty"`
+	SupportsAcpi      bool      `json:"SupportsAcpi,omitempty"`
+	SupportsApm       bool      `json:"SupportsApm,omitempty"`
+	SupportsAgp       bool      `json:"SupportsAgp,omitempty"`
+	SupportsPcmcia    bool      `json:"SupportsPcmcia,omitempty"`
+	HasSmartBattery   bool      `json:"HasSmartBattery,omitempty"`
+	SupportsUefi      bool      `json:"SupportsUefi,omitempty"`
+	SupportsLegacyUsb bool      `json:"SupportsLegacyUsb,omitempty"`
+	SupportsPci       bool      `json:"SupportsPci,omitempty"`
+	SupportsVlvesa    bool      `json:"SupportsVlvesa,omitempty"`
+	SupportsEscd      bool      `json:"SupportsEscd,omitempty"`
+	SupportsNetBoot   bool      `json:"SupportsNetBoot,omitempty"`
+	SupportsI2OBoot   bool      `json:"SupportsI2OBoot,omitempty"`
+	IsVirtualMachine  bool      `json:"IsVirtualMachine,omitempty"`
+	PowerOnReason     string    `json:"PowerOnReason,omitempty"`
+	IsPortable        bool      `json:"IsPortable,omitempty"`
+	VmHost            string    `json:"VmHost,omitempty"`
+	VmType            string    `json:"VmType,omitempty"`
+	VmName            string    `json:"VmName,omitempty"`
+	DateUpdated       time.Time `json:"DateUpdated,omitzero"`
 }
 
 // ComputerDrive is a disk volume on a computer (LabTech.Models.ComputerDrive).
@@ -259,8 +328,11 @@ type ComputerDrive struct {
 	IsMissing           bool      `json:"IsMissing,omitempty"`
 	DateLastInventoried time.Time `json:"DateLastInventoried,omitzero"`
 	VolumeName          string    `json:"VolumeName,omitempty"`
+	BackupFlag          int       `json:"BackupFlag,omitempty"`
 	IsSolidState        bool      `json:"IsSolidState,omitempty"`
 	IsInternal          bool      `json:"IsInternal,omitempty"`
+
+	MaximumHistoryDaysAvailable int64 `json:"MaximumHistoryDaysAvailable,omitempty"`
 }
 
 // ComputerPatchingStats summarizes a computer's patch compliance
@@ -274,12 +346,22 @@ type ComputerPatchingStats struct {
 	CompliantSoftwareCount    int       `json:"CompliantSoftwareCount,omitempty"`
 	NonCompliantSoftwareCount int       `json:"NonCompliantSoftwareCount,omitempty"`
 	FailedSoftwareCount       int       `json:"FailedSoftwareCount,omitempty"`
+	IncorrectSoftwareCount    int       `json:"IncorrectSoftwareCount,omitempty"`
 	Stage                     string    `json:"Stage,omitempty"`
+	NoPatchInventory          bool      `json:"NoPatchInventory,omitempty"`
 	WSUSEnabled               bool      `json:"WSUSEnabled,omitempty"`
 	PatchJobRunning           bool      `json:"PatchJobRunning,omitempty"`
+	DaytimePatchingEnabled    bool      `json:"DaytimePatchingEnabled,omitempty"`
+	WUAOutOfDate              bool      `json:"WUAOutOfDate,omitempty"`
+	MissingBaselinePatches    bool      `json:"MissingBaselinePatches,omitempty"`
+	WUAVersion                string    `json:"WUAVersion,omitempty"`
 	LastInstallWindow         time.Time `json:"LastInstallWindow,omitzero"`
 	NextInstallWindow         time.Time `json:"NextInstallWindow,omitzero"`
+	LastSoftwareWindow        time.Time `json:"LastSoftwareWindow,omitzero"`
+	NextSoftwareWindow        time.Time `json:"NextSoftwareWindow,omitzero"`
 	LastPatchedDate           time.Time `json:"LastPatchedDate,omitzero"`
+	LastMicrosoftPatchedDate  time.Time `json:"LastMicrosoftPatchedDate,omitzero"`
+	LastThirdPartyPatchedDate time.Time `json:"LastThirdPartyPatchedDate,omitzero"`
 	LastPatchInventory        time.Time `json:"LastPatchInventory,omitzero"`
 	IsMicrosoftManaged        bool      `json:"IsMicrosoftManaged,omitempty"`
 	IsThirdPartyManaged       bool      `json:"IsThirdPartyManaged,omitempty"`
